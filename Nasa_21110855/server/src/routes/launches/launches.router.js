03d62066ed9bf5cd0cdec92ba0910e15509a0972
ThienDang1409 <145652737+ThienDang1409@users.noreply.github.com>
2023-11-05 13:29:49 +0700

    update
    a

---
+++ Nasa_21110855/server/src/routes/launches/launches.router.js
@@ -0,0 +1,6 @@
+const {launches} = require('../../models/launches.model');
+const launchesRouter = express.Router();
+
+launchesRouter.get('/launches', getAllLaunches);
+
+module.exports = launchesRouter;
